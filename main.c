6e7f2127414ae4e0ce16937e9dd561757df7154b
Chris Kim <sunchaesk@gmail.com>
2022-12-23 22:08:04 -0500

    curr

--- main.c
+++ main.c
@@ -2,10 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include "util.h"
 
 enum TokenType {
 COMBINATOR,
+IDENTIFIER,
 LEFT_PAREN,
 RIGHT_PAREN
 };
@@ -18,6 +20,7 @@ typedef struct {
 typedef struct{
     Token * tokens;
     size_t tokens_len;
+    size_t curr_index;
 } Lexer;
 
 Lexer lex(char *input) {
@@ -41,6 +44,10 @@ Lexer lex(char *input) {
             tokens[token_index].type = RIGHT_PAREN;
             tokens[token_index].value = current_char;
             token_index++;
+        } else if (isalpha(current_char) && current_char != 'S' && current_char != 'K' && current_char != 'I'){
+            tokens[token_index].type = IDENTIFIER;
+            tokens[token_index].value = current_char;
+            token_index++;
         }
     char_index++;
     current_char = input[char_index];
@@ -48,8 +55,49 @@ Lexer lex(char *input) {
     Lexer ret_lexer;
     ret_lexer.tokens = tokens;
     ret_lexer.tokens_len = token_index;
+    ret_lexer.curr_index = 0;
     return ret_lexer;
 }
+
+////////////////////////////////////////////////////
+///                   Parse                   /////
+////////////////////////////////////////////////////
+typedef struct ASTnode {
+    char type;
+    struct ASTnode *left;
+    struct ASTnode *right;
+} ASTnode;
+
+struct S {
+    char type;
+    char first;
+    char second;
+    char third;
+};
+
+struct K {
+    char type;
+    char first;
+    char second;
+};
+
+struct I {
+    char type;
+
+};
+
+ASTnode* new_ASTnode(char type, ASTnode* left, ASTnode* right){
+    ASTnode * node = malloc(sizeof(ASTnode));
+    node->type = type;
+    node->left = left;
+    node->right = right;
+    return node;
+}
+
+
+
+
+
 
 int main(int argc, char *argv[]){
     FILE *file;
@@ -67,7 +115,7 @@ int main(int argc, char *argv[]){
 
     char * file_contents = read_file(file);
     Lexer toks = lex(file_contents);
-    for (int i = 0; i < toks.tokens_len; i++){
+    for (size_t i = 0; i < toks.tokens_len; i++){
         printf("%c\n", toks.tokens[i].value);
     }
 
